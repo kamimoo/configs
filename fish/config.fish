1a23f5c1095fc99d8da5cecba822977e9934bebd
kamimoo <arcobaleno7+git@gmail.com>
2018-03-26 17:03:59 +0900

    Add fish settings

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+set -x EDITOR nvim
+
+# android
+if test -e $HOME/Library/Android/sdk
+	set -x ANDROID_HOME $HOME/Library/Android/sdk
+	set -x PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
+end
+
+if test -e $HOME/.nodebrew
+	set -x PATH $PATH $HOME/.nodebrew/current/bin
+end
+
+# Homebrew
+set -x HOMEBREW_CASK_OPTS '--appdir=/Applications'
+set -x HOMEBREW_NO_ANALYTICS 1
+
+# direnv
+eval (direnv hook fish)
+
+# nodenv
+status --is-interactive; and source (nodenv init -|psub)
