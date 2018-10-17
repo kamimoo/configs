50a8330b8c9da1f575aa5e862cb224925bc6a172
kamimoo <arcobaleno7+git@gmail.com>
2018-10-17 22:43:29 +0900

    Install Fisher v3

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,13 @@
 set -x EDITOR nvim
 
+# fisherman
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # android
 if test -e $HOME/Library/Android/sdk
 	set -x ANDROID_HOME $HOME/Library/Android/sdk
